0231c4e6d283179c52529bb5311c891dc4483d35
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-10-05 13:52:17 +0500

    create and show added to tournaments

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,13 +1,37 @@
 class TournamentsController < ApplicationController
+  before_action :set_tournament, only: [:show, :edit, :update, :destroy]
   before_action :set_new_tournament, only: [:new, :create]
 
   def index
 
   end
 
+  def show
+
+  end
+
   def new; end
 
+  def create
+    @tournament.assign_attributes(tournament_params)
+
+    if @tournament.save
+      redirect_to tournament_url(@tournament), notice: t(".success")
+    else
+      flash.now[:alert] = @tournament.errors.full_messages
+      render 'new'
+    end
+  end
+
   private
+
+  def tournament_params
+    params.require(:tournament).permit(:name)
+  end
+
+  def set_tournament
+    @tournament = Tournament.find(params[:id])
+  end
 
   def set_new_tournament
     @tournament = Tournament.new
