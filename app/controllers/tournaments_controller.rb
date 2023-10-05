f8698db3fa879126060d8dd47eee6d789d253706
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-10-05 13:33:49 +0500

    tournament model and new form added

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,7 +1,15 @@
 class TournamentsController < ApplicationController
+  before_action :set_new_tournament, only: [:new, :create]
+
   def index
 
   end
 
   def new; end
+
+  private
+
+  def set_new_tournament
+    @tournament = Tournament.new
+  end
 end
